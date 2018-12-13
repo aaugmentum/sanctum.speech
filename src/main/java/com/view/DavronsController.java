94845029ecb3a93045be746dd2dc9fa274772e0c
enorenio <morshnev.aleksey@gmail.com>
2018-12-14 01:37:58 +0500

    playVideo is now easier with videoQueue

--- src/main/java/com/view/DavronsController.java
+++ src/main/java/com/view/DavronsController.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.scene.input.MouseEvent;
 
 import java.io.File;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class DavronsController extends ViewController {
 
@@ -27,7 +29,52 @@ public class DavronsController extends ViewController {
         // when request returned anything call setVoiceLabelIdle();
         // then in order to add recognized text use addCell(String message);
         // then after recognized use playVideo();
-        addCell("Recognized text");
-        playVideo(new File("assets/small.mp4"));
+        afterRecognized();
+//        addCell("Recognized text");
+//        playVideo(new File("assets/small.mp4"));
+
+    }
+
+    @Override
+    public void beforeVideo() {
+        super.beforeVideo();
+
+    }
+
+    @Override
+    public void afterVideo() {
+        super.afterVideo();
+        try{
+            playNext();
+        }catch(java.lang.NullPointerException e){
+            stopVideo();
+        }
+    }
+
+    Queue<File> videoQueue = new LinkedList<>();
+
+    public void playNext() {
+
+        // These 5 lines add the file name to the listView.
+        // You can Either leave them or delete and use addCell(recognized_text_string);
+        String nextVideoName = videoQueue.peek().getName();
+        if (nextVideoName.indexOf(".") > 0) {
+            nextVideoName = nextVideoName.substring(0, nextVideoName.lastIndexOf("."));
+        }
+        addCell(nextVideoName);
+        playVideo(videoQueue.poll());
+    }
+
+    public void afterRecognized() {
+        // assume you have to play 3 videos
+        File[] videos = {
+                new File("assets/small.mp4"),
+                new File("assets/small.mp4"),
+                new File("assets/small.mp4"),
+        };
+        // Add all videos to queue
+        for(File video : videos) videoQueue.add(video);
+        // Call this function once, then it will be called autimatically in "afterVideo()"
+        playNext();
     }
 }
